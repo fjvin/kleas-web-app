585de71ad65802e57aec5cdc626f3b8a529aff19
fjvin <qfveatabay@tip.edu.ph>
2022-10-24 14:57:39 +0800

    add edit and delete sales capability

--- sales/views.py
+++ sales/views.py
@@ -1,12 +1,33 @@
 from typing import List
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, UpdateView, DeleteView
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse_lazy, reverse
 from .models import Sale, Item, Category, PaymentType
 
 
+class SaleTransactions(ListView):
+    model = Sale
+    queryset = Sale.objects.order_by("-purchase_date")
+    template_name = "sales/transactions.html"
+    context_object_name = "sales"
+
+class SaleEdit(UpdateView):
+    model = Sale
+    fields = ["price", "quantity", "payment", "category", "item"]
+    template_name = "sales/add.html"
+    extra_context = {"title": "Edit Sale"}
+    success_url = reverse_lazy("sales:transactions")
+
+class SaleDelete(DeleteView):
+    model = Sale
+    success_url = reverse_lazy("sales:transactions")
+    template_name = "sales/delete.html"
+
 def add(request):
+
+    context = {"title": "Add Sales"}
+
     if request.method == "POST":
         saleObj = Sale()
         saleObj.price = request.POST["price"]
@@ -20,20 +41,10 @@ def add(request):
 
         return redirect(reverse("sales:add"))
     else:
-        return render(request, "sales/add.html")
-
-
-class SaleTransactions(ListView):
-    model = Sale
-    queryset = Sale.objects.order_by("-purchase_date")
-    template_name = "sales/transactions.html"
-    context_object_name = "sales"
+        return render(request, "sales/add.html", context=context)
 
 
 def load_items(request):
     category_id = request.GET.get('category')
     items = Item.objects.filter(category_id=category_id).order_by('item')
     return render(request, 'sales/item_options.html', context={'items': items})
-
-def success(request):
-    return render(request, 'sales/success.html')
