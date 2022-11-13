67148e83320ee71efc8c91bde6ce93123c5e4cc8
fjvin <qfveatabay@tip.edu.ph>
2022-11-13 22:33:21 +0800

    add edit sales

--- kleas/sales/urls.py
+++ kleas/sales/urls.py
@@ -5,6 +5,8 @@ app_name = 'sales'
 
 urlpatterns = [
     path('', views.SaleCreateView.as_view(), name='add'),
+    path('transactions/', views.SaleListView.as_view(), name='transactions'),
+    path('update_sale/<int:pk>', views.SaleUpdateView.as_view(), name='update_sale'),
 
     # endpoint where ajax updates clothes items based on category
     path('ajax/load-items/', views.load_items, name='ajax_load_items'),
--- kleas/sales/views.py
+++ kleas/sales/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, UpdateView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
@@ -13,6 +13,17 @@ class SaleCreateView(CreateView):
     template_name = 'sales/sale_form.html'
     success_url = reverse_lazy('sales:add')
 
+class SaleListView(ListView):
+    model = Sale
+    queryset = Sale.objects.order_by('-purchase_date')
+    template_name = 'sales/transactions.html'
+    context_object_name = 'sales'
+
+class SaleUpdateView(UpdateView):
+    model = Sale
+    template_name = 'sales/edit_sale_form.html'
+    form_class = SaleForm
+    success_url = reverse_lazy('sales:transactions')
 
 def load_items(request):
     category_id = request.GET['category']
