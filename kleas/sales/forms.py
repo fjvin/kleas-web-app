833fdb5e49e136efae2739068f847769dfedb0f7
fjvin <qfveatabay@tip.edu.ph>
2022-11-13 21:50:22 +0800

    add form errors inital

--- kleas/sales/forms.py
+++ kleas/sales/forms.py
@@ -24,34 +24,38 @@ class SaleForm(forms.ModelForm):
                     "class": "form-control", 
                     "min":"1",
                     "value": "1",
+                    "required": True
                 }
             ),
             "payment": forms.Select(
                 attrs={
                     "id": "payment-dropdown",
-                    "class": "selectpicker show-tick",
+                    "class": "selectpicker show-tick form-control",
                     "title":"Select Payment",
                     "data-width": "100%",
                     "data-style" :"btn-outline-primary",
+                    "required": True
                 },
             ),
 
             "category": forms.Select(
                 attrs={
                     "id": "category-dropdown",
-                    "class": "selectpicker show-tick",
+                    "class": "selectpicker show-tick form-control",
                     "title":"Select Category",
                     "data-width": "100%",
                     "data-style" :"btn-outline-primary",
+                    "required": True
                 },
             ),
             "item": forms.Select(
                 attrs={
                     "id": "item-dropdown",
-                    "class": "selectpicker show-tick",
+                    "class": "selectpicker show-tick form-control",
                     "title":"Select Item",
                     "data-width": "100%",
                     "data-style" :"btn-outline-primary",
+                    "required": True
                 },
             ),
         }
