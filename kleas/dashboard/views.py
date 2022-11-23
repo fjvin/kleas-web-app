1cc65d8485ed5fdf53c09d36d4d342a40217a9b3
fjvin <qfveatabay@tip.edu.ph>
2022-11-23 23:28:48 +0800

    add revenue trend and expenses trend

--- kleas/dashboard/views.py
+++ kleas/dashboard/views.py
@@ -2,6 +2,7 @@ from django_pandas.io import read_frame
 from django.shortcuts import render
 from expenses.models import ExpensesRestock, ExpensesStore
 from django.contrib.auth.decorators import login_required
+import plotly.graph_objects as go
 from sales.models import Sale
 import plotly.express as px
 import pandas as pd
@@ -14,16 +15,23 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 @login_required
 def dashboard(request):
     
+    # load database models
     sales = Sale.objects.all()
     restock_expenses = ExpensesRestock.objects.all()
     store_expenses = ExpensesStore.objects.all()
 
+    # convert Sale model into dataframes
     sales_df = read_frame(sales)
     sales_df.purchase_date = pd.to_datetime(
-        sales_df['purchase_date'],format='%m/%d/%y %I:%M%p')
+        sales_df['purchase_date'], format='%m/%d/%y %I:%M%p')
 
+    # convert Expenses model into dataframes
     restock_expenses_df = read_frame(restock_expenses)
+    restock_expenses_df.purchase_date = pd.to_datetime(
+        restock_expenses_df['purchase_date'], format='%m/%d/%y %I:%M%p')
     store_expenses_df = read_frame(store_expenses)
+    store_expenses_df.date = pd.to_datetime(
+        store_expenses_df['date'])
 
     # compute for gross income
     revenue = get_revenue(sales_df)
@@ -34,9 +42,12 @@ def dashboard(request):
     total_num_sales = get_total_sales(sales_df)
     total_num_expenses = get_total_num_expenses(restock_expenses_df, store_expenses_df)
 
-
-    sales_graph = get_sales_trend_graph(sales_df)
+    # generate graphs
+    sales_trend_graph = get_sales_trend_graph(sales_df)
     category_item_graph = get_category_item_breakdown_graph(sales_df)
+    revenue_trend_graph = get_revenue_trend_graph(sales_df)
+
+    expenses_trend_graph = get_expenses_trend_graph(restock_expenses_df, store_expenses_df)
 
     context = {
         'revenue': revenue,
@@ -46,12 +57,16 @@ def dashboard(request):
         'total_num_sales': total_num_sales,
         'total_num_expenses': total_num_expenses,
 
-        'sales_graph' : sales_graph,
+        'revenue_trend_graph': revenue_trend_graph,
+        'expenses_trend_graph': expenses_trend_graph,
+        'sales_trend_graph' : sales_trend_graph,
         'category_item_graph': category_item_graph,
     }
     return render(request, 'dashboard/dashboard.html', context=context)
 
 def get_revenue(df):
+
+    # compute for total revenue
     df['total_price'] = df['price'] * df['quantity']
     revenue = float(df['total_price'].sum())
     return revenue
@@ -70,22 +85,96 @@ def get_expenses(restock, store):
 
 
 def compute_gross_income(revenue, expenses):
+
+    # compute for gross income
     gross_income = revenue - expenses
     return gross_income
 
 
 def get_total_sales(df):
+
+    # aggregate total number of sales
     total_sales = int(df['quantity'].sum())
     return total_sales
 
 
 def get_total_num_expenses(restock, store):
+
+    # aggrate total number of expenses
     total_expenses = int(restock['quantity'].sum())
     total_expenses += store.shape[0]
     return total_expenses
 
 
+def get_revenue_trend_graph(df):
+
+    # compute for total price
+    df['total_price'] = df['price'] * df['quantity']
+
+    # aggregate revenue per day
+    revenue_per_day = df[['purchase_date', 'total_price']].resample(
+        'D', on='purchase_date').sum().reset_index()
+    
+    fig = px.bar(revenue_per_day, 
+                x='purchase_date',
+                y='total_price',
+                labels={
+                    "value": "Sales",
+                    "purchase_date": "Date",
+                    "total_price": "Total Revenue (₱)"
+                    }, 
+                title='Revenue Trend'
+                )
+
+    fig.add_trace(go.Scatter(
+                x=revenue_per_day.purchase_date,
+                y=revenue_per_day.total_price,
+                showlegend=False
+            ))
+
+    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+def get_expenses_trend_graph(restock, store):
+    
+    # compute total price
+    restock['total_price'] = restock['price'] * restock['quantity']
+    # aggregate restock expenses per month
+    restock_expenses_per_month = restock[['purchase_date', 'total_price']].resample(
+        'M', on='purchase_date').sum().reset_index()
+    restock_expenses_per_month['purchase_date'] = restock_expenses_per_month['purchase_date'].apply(
+        lambda dt: dt.replace(tzinfo=None))
+    
+    # aggregate store expenses per month
+    store_expenses_per_month = store[['date', 'amount']].resample(
+        'M', on='date').sum().reset_index()
+    
+
+    merge_data = pd.merge(restock_expenses_per_month, 
+                          store_expenses_per_month, 
+                          how='outer',left_on='purchase_date',
+                          right_on='date')
+    
+    merge_data['total_amount'] = merge_data['total_price'] + merge_data['amount']
+    merge_data = merge_data[['date', 'total_amount']]
+
+    fig = px.bar(merge_data, 
+                x='date',
+                y='total_amount',
+                labels={
+                    "date": "Date",
+                    "total_amount": "Total Expenses (₱)"
+                    }, 
+                title='Expenses Trend',
+                barmode='group')
+                
+    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+
 def get_sales_trend_graph(df):
+
+    # generate total # of sales per category
+
     sales_df = df[['purchase_date', 'category', 'quantity']]
     sales_df = df.assign(key=df.groupby('category').cumcount()).pivot(
         'purchase_date','category','quantity')
@@ -107,7 +196,11 @@ def get_sales_trend_graph(df):
 
 
 def get_category_item_breakdown_graph(df):
+
+    # generate total sales by category and item
+
     fig = px.sunburst(df, path=['category', 'item'], 
                       values='quantity', 
                       title='Sales by Category and Item')
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
